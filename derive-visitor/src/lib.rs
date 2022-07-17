9b12b60fded517530f6847e8a5b2c427a10d3276
Alex Badics <admin@stickman.hu>
2022-07-17 21:08:57 +0200

    Replace Drive impl for collections with a more generic impl
    This is an incompatible change, since a lot of new types have been
    impl'd, which might have been impl'd by others (probably not, but let's
    be correct about this)
    
    As a bonus, it now works for bigger than 10 element arrays too.

--- derive-visitor/src/lib.rs
+++ derive-visitor/src/lib.rs
@@ -89,12 +89,7 @@ pub use derive_visitor_macros::Drive;
 /// See [Visitor].
 pub use derive_visitor_macros::Visitor;
 
-use std::marker::PhantomData;
-use std::{
-    any::Any,
-    cell::Cell,
-    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque},
-};
+use std::{any::Any, cell::Cell, marker::PhantomData};
 
 #[cfg(feature = "std-types-drive")]
 use std::ops::{Range, RangeBounds, RangeFrom, RangeInclusive, RangeTo, RangeToInclusive};
@@ -367,34 +362,42 @@ pub trait Drive: Any {
     fn drive<V: Visitor>(&self, visitor: &mut V);
 }
 
-impl<K, Val> Drive for BTreeMap<K, Val>
+// Helper trait to the generic `IntoIterator` Drive impl
+trait DerefAndDrive {
+    fn deref_and_drive<V: Visitor>(self, visitor: &mut V);
+}
+
+// Most collections iterate over item references, this is the trait impl that handles that case
+impl<T: Drive> DerefAndDrive for &T {
+    fn deref_and_drive<V: Visitor>(self, visitor: &mut V) {
+        self.drive(visitor);
+    }
+}
+
+// Map-like collections iterate over item references pairs
+impl<TK: Drive, TV: Drive> DerefAndDrive for (&TK, &TV) {
+    fn deref_and_drive<V: Visitor>(self, visitor: &mut V) {
+        self.0.drive(visitor);
+        self.1.drive(visitor);
+    }
+}
+
+// The following should be able to handle almost all container types:
+// array, BTreeMap, BTreeSet, BinaryHeap, slice, HashMap, HashSet, LinkedList, Vec, VecDeque
+//
+// It even handles Option, since it has a trivial IntoIterator impl.
+impl<T> Drive for T
 where
-    K: Drive,
-    Val: Drive,
+    T: 'static,
+    // HRTB, because this needs to be true for late-bound lifetimes,
+    // as both &self and &visitor have anonymous lifetimes.
+    for<'a> &'a T: IntoIterator,
+    for<'a> <&'a T as IntoIterator>::Item: DerefAndDrive,
 {
     fn drive<V: Visitor>(&self, visitor: &mut V) {
-        for (key, value) in self.iter() {
-            key.drive(visitor);
-            value.drive(visitor);
+        for item in self {
+            item.deref_and_drive(visitor);
         }
-    }
-}
-
-impl<T> Drive for BTreeSet<T>
-where
-    T: Drive,
-{
-    fn drive<V: Visitor>(&self, visitor: &mut V) {
-        self.iter().for_each(|item| item.drive(visitor));
-    }
-}
-
-impl<T> Drive for BinaryHeap<T>
-where
-    T: Drive,
-{
-    fn drive<V: Visitor>(&self, visitor: &mut V) {
-        self.iter().for_each(|item| item.drive(visitor));
     }
 }
 
@@ -406,75 +409,12 @@ where
         (**self).drive(visitor);
     }
 }
-
 impl<T> Drive for Cell<T>
 where
     T: Drive + Copy,
 {
     fn drive<V: Visitor>(&self, visitor: &mut V) {
         self.get().drive(visitor);
-    }
-}
-
-impl<K, Val, S> Drive for HashMap<K, Val, S>
-where
-    K: Drive,
-    Val: Drive,
-    S: 'static,
-{
-    fn drive<V: Visitor>(&self, visitor: &mut V) {
-        for (key, value) in self.iter() {
-            key.drive(visitor);
-            value.drive(visitor);
-        }
-    }
-}
-
-impl<T, S> Drive for HashSet<T, S>
-where
-    T: Drive,
-    S: 'static,
-{
-    fn drive<V: Visitor>(&self, visitor: &mut V) {
-        self.iter().for_each(|item| item.drive(visitor));
-    }
-}
-
-impl<T> Drive for LinkedList<T>
-where
-    T: Drive,
-{
-    fn drive<V: Visitor>(&self, visitor: &mut V) {
-        self.iter().for_each(|item| item.drive(visitor));
-    }
-}
-
-impl<T> Drive for Option<T>
-where
-    T: Drive,
-{
-    fn drive<V: Visitor>(&self, visitor: &mut V) {
-        if let Some(value) = self {
-            value.drive(visitor);
-        }
-    }
-}
-
-impl<T> Drive for Vec<T>
-where
-    T: Drive,
-{
-    fn drive<V: Visitor>(&self, visitor: &mut V) {
-        self.iter().for_each(|item| item.drive(visitor));
-    }
-}
-
-impl<T> Drive for VecDeque<T>
-where
-    T: Drive,
-{
-    fn drive<V: Visitor>(&self, visitor: &mut V) {
-        self.iter().for_each(|item| item.drive(visitor));
     }
 }
 
@@ -510,41 +450,6 @@ tuple_impls! {
     T0, T1, T2, T3, T4, T5 => 0, 1, 2, 3, 4, 5
     T0, T1, T2, T3, T4, T5, T6 => 0, 1, 2, 3, 4, 5, 6
     T0, T1, T2, T3, T4, T5, T6, T7 => 0, 1, 2, 3, 4, 5, 6, 7
-}
-
-impl<T> Drive for [T; 0]
-where
-    T: Drive,
-{
-    fn drive<V: Visitor>(&self, _visitor: &mut V) {}
-}
-
-macro_rules! array_impls {
-    ( $( $len:expr => $( $field:expr ),+ )+ ) => {
-        $(
-            impl<T> Drive for [T; $len]
-            where
-                T: Drive
-            {
-                fn drive<V: Visitor>(&self, visitor: &mut V) {
-                    $(
-                        self[$field].drive(visitor);
-                    )+
-                }
-            }
-        )+
-    };
-}
-
-array_impls! {
-    1 => 0
-    2 => 0, 1
-    3 => 0, 1, 2
-    4 => 0, 1, 2, 3
-    5 => 0, 1, 2, 3, 4
-    6 => 0, 1, 2, 3, 4, 5
-    7 => 0, 1, 2, 3, 4, 5, 6
-    8 => 0, 1, 2, 3, 4, 5, 6, 7
 }
 
 #[cfg(feature = "std-types-drive")]
